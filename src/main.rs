354c31382058effc3e61d403a32550e8621a23e1
Nicolò Lazzaroni <55408375+NicoloLazzaroni@users.noreply.github.com>
2023-11-17 17:17:01 +0100

    Added the ability to bind to a specific IP.

--- src/main.rs
+++ src/main.rs
@@ -50,7 +50,7 @@ pub struct Shim {
 impl Shim {
 	/// Creates a new Shim with the specified local address to listen on and the remote address to relay connections to.
 	fn new(listen_addr: SocketAddr, connect_addr: SocketAddr, config: AppConfig) -> Res<Shim> {
-		let real_listen_addr = (config.bind_to, listen_addr.port())
+		let real_listen_addr = (config.bind_to.clone(), listen_addr.port())
 			.to_socket_addrs()?
 			.next()
 			.unwrap();
@@ -180,7 +180,7 @@ fn main() -> Res<()> {
 		std::process::exit(1);
 	});
 
-	let listen_addr = ("0.0.0.0", config.external_auth_port)
+	let listen_addr = (config.bind_to.clone(), config.external_auth_port)
 		.to_socket_addrs()
 		.unwrap()
 		.next()
